3cc3e69a2af68b295544cea3244a6f1529f4667e
Sidney Ramos <sidney.ramos95@gmail.com>
2021-07-06 22:23:57 +1000

    Removed webpack stuff

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ const path = require("path");
 
 module.exports = {
   target: "serverless",
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  },
 };
