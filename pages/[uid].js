ec7bcb4b9fe7cc50db5007147add1eba04dbbbc1
Sidney Ramos <sidney.ramos95@gmail.com>
2021-07-06 21:50:55 +1000

    Trial

--- pages/[uid].js
+++ pages/[uid].js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import DefaultLayout from "layouts";
-import SliceZone from "components/slices/SliceZone";
+import SliceZone from "../components/slices/SliceZone.js";
 
 import { queryRepeatableDocuments } from "utils/queries";
 
